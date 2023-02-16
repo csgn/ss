6c9893a6dce06e8ac2c2ab4d3dfdb08da629ee7b
csgn <dev.csgn@gmail.com>
2023-02-16 20:40:47 +0300

    refactor

---
+++ 10352/10352-users-by-avg-session-time.py
@@ -0,0 +1,8 @@
+# Users By Average Session Time ( 10352 ) ( Medium )
+# https://platform.stratascratch.com/coding/10352-users-by-avg-session-time?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+facebook_web_log.head()
