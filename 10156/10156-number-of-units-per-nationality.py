e3db7cf682d0b7af224cd6d734ceb398320acc3e
csgn <dev.csgn@gmail.com>
2023-02-17 19:26:00 +0300

    INIT 10156

---
+++ 10156/10156-number-of-units-per-nationality.py
@@ -0,0 +1,8 @@
+# Number Of Units Per Nationality ( 10156 ) ( Medium )
+# https://platform.stratascratch.com/coding/10156-number-of-units-per-nationality?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+airbnb_hosts.head()
---
+++ 10156/10156-number-of-units-per-nationality.sql
@@ -0,0 +1,6 @@
+-- Number Of Units Per Nationality ( 10156 ) ( Medium )
+-- https://platform.stratascratch.com/coding/10156-number-of-units-per-nationality?code_type=1
+select
+  *
+from
+  airbnb_hosts;
